533dacbcde4def398a6cb8cc499bd8ebfebb13d0
akinwunmi-akinrimisi <akinolaakinrimisi@gmail.com>
2024-09-17 17:23:54 +0100

    functiono to register player added

---
+++ contracts/Ludo.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract LudoGame {
+   
+    uint8 public constant maxNumberOfPlayers = 4;       // number of players and their positions
+    uint8 public constant totalSpaceOnBoard = 52;       //  board size
+    uint8 public currentPlayerIndex = 0;                // To track whose turn it is
+    bool public gameStarted = false;
+    bool public gameEnded = false;
+    
+    address[] public players;                           // Array of players
+    mapping(address => uint8) public playerPositions;   // Tracking player’s position on the board
+
+  
+
+    // Event to indicate a player rolled the dice and moved
+    event DiceRolled(address indexed player, uint8 diceResult, uint8 newPosition);
+
+
+    // Function for players to register
+    function registerPlayer() public {
+        require(players.length < maxNumberOfPlayers, "Maximum players reached.");
+        require(!gameStarted, "Game has already started.");
+        require(playerPositions[msg.sender] == 0, "Player is already registered.");
+
+        players.push(msg.sender); // Add player to the list
+        playerPositions[msg.sender] = 1; // Initial position is 1 (starting point)
+    }
+
+}
+
